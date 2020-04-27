35080b3898e40398af91a6fe0a92839723948aea
Asaga Kosho <asagatto777@gmail.com>
2020-04-27 21:43:40 +0900

    [Update] Change Logos & Add Variations of Logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#0287fe`,
         display: `minimal-ui`,
-        icon: `src/images/samepro-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/circle_blue.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
