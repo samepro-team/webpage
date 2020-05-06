217e70df365c924bf06fc15b689065242a80b6a1
Asaga Kosho <asagatto777@gmail.com>
2020-05-06 22:55:56 +0900

    [Update] 軽微な修正

--- src/pages/tos.js
+++ src/pages/tos.js
@@ -2,7 +2,6 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import MainImage from "../components/MainImage"
 import SEO from "../components/seo"
 
 const Tos = () => {
